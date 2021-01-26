9f978dbd30008cd26561d0193be4d402bee136be
René Meye <rene@vesputi.com>
2021-01-26 16:18:24 +0100

    feat(random api): added the ability to return multiple random items at once
    You only have to give the random function a number, and it returns you a list of random server names
    
    closes #1

--- src/index.test.js
+++ src/index.test.js
@@ -23,8 +23,30 @@ describe('server-names', function(){
   })
 
   describe('random API', function(){
-    it('returns a string', function(){
+    it('returns a string if passed nothing', function(){
       expect(typeof server_names.random()).to.eq("string")
+    })
+
+    it('returns an array of strings if passed a number', function(){
+      it('returns an array in the length of the parameter', function(){
+        expect(server_names.random(3).length).to.eq(3);
+      })
+
+      it('returns an array of strings if given a number', function(){
+        expect(server_names.random(3)).to.satisfy(isArrayOfStrings);
+
+        function isArrayOfStrings(value){
+          return Array.isArray(value) && value.every(function(item){
+              return typeof item === 'string'
+          })
+        }
+      })
+
+      it('returns an array of strings that are included in the names list', function(){
+        server_names.random(15).forEach((item, i) => {
+          expect(server_names.all).to.contain(item)
+        });
+      })
     })
   })
 
