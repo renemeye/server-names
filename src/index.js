2ad714e89f9ba9b6e0a081bfa7a8aa5d050e2ef1
René Meye <rene@vesputi.com>
2021-01-28 22:47:00 +0100

    chore(test-coverage): adds a check for test-coverage
    testing it with istanbul

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ const server_name_list = require('./server-names.json');
 module.exports = {
   all: server_name_list,
   get_random: (count)=>(!!count)?[...Array(count)].map(uniqueRandomArray(server_name_list)):uniqueRandomArray(server_name_list)()
-
 };
