69dddb5f73d4d55c8732ce95fad56d4b302b59b0
René Meye <rene@vesputi.com>
2021-01-28 22:23:58 +0100

    feat(ramdom api): renames the random to get_random function
    nope
    
    BREAKING CHANGE: The name of random changes to get_random

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const server_name_list = require('./server-names.json');
 
 module.exports = {
   all: server_name_list,
-  random: uniqueRandomArray(server_name_list)
+  get_random: (count)=>(!!count)?[...Array(count)].map(uniqueRandomArray(server_name_list)):uniqueRandomArray(server_name_list)()
+
 };
--- src/index.test.js
+++ src/index.test.js
@@ -24,29 +24,30 @@ describe('server-names', function(){
 
   describe('random API', function(){
     it('returns a string if passed nothing', function(){
-      expect(typeof server_names.random()).to.eq("string")
+      expect(typeof server_names.get_random()).to.eq("string")
     })
 
     it('returns an array of strings if passed a number', function(){
-      it('returns an array in the length of the parameter', function(){
-        expect(server_names.random(3).length).to.eq(3);
-      })
+      expect(server_names.get_random(3).length).to.eq(3);
+    })
 
-      it('returns an array of strings if given a number', function(){
-        expect(server_names.random(3)).to.satisfy(isArrayOfStrings);
+    it('returns an array of strings if given a number', function(){
+      expect(server_names.get_random(3)).to.satisfy(isArrayOfStrings);
 
-        function isArrayOfStrings(value){
-          return Array.isArray(value) && value.every(function(item){
-              return typeof item === 'string'
-          })
-        }
-      })
+      function isArrayOfStrings(value){
+        return Array.isArray(value) && value.every(function(item){
+            return typeof item === 'string'
+        })
+      }
+    })
 
-      it('returns an array of strings that are included in the names list', function(){
-        server_names.random(15).forEach((item, i) => {
-          expect(server_names.all).to.contain(item)
-        });
-      })
+    it('returns an array of strings that are included in the names list', function(){
+      var counter = 0
+      server_names.get_random(15).map((item) => {
+        counter++
+        expect(server_names.all).to.contain(item)
+      });
+      expect(counter).to.eq(15);
     })
   })
 
