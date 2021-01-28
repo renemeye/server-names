a907e1a22f74dc814e936fef9a06f356c440c73f
René Meye <rene@vesputi.com>
2021-01-28 23:52:50 +0100

    build(babel es6): adds ES6 support
    Gives a ES6 support via all the cool ways thinkable including only shipping the compiled dist
    directiory in npm packag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const uniqueRandomArray = require('unique-random-array');
-const server_name_list = require('./server-names.json');
+import uniqueRandomArray from 'unique-random-array';
+import server_name_list from './server-names.json';
 
 module.exports = {
   all: server_name_list,
